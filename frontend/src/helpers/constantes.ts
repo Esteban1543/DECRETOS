abfe81fd770f7adb2739d6f5e16052049df75b82
Diego Ladino <galatas96@gmail.com>
2024-06-17 21:21:53 -0500

    ✅ datos de origenes y procesos actualizados

--- frontend/src/helpers/constantes.ts
+++ frontend/src/helpers/constantes.ts
@@ -1,17 +1,21 @@
 export const origenes = [
-  'Juzgado Sexto de Pequeñas Causas Civiles y Competencia Múltiple de Bogotá DC',
   'Juzgado Primero Municipal de Pequeñas Causas Civiles y Competencia Múltiple de Tunja',
+  
+  //enviados por el profe
+  'Juzgado Sexto de Pequeñas Causas Civiles y Competencia Múltiple de Bogotá D.C.',
+  'Juzgado Ochenta Civil Municipal de Bogotá D.C. Transitorio Sesenta y Dos (62) de Pequeñas Causas Civiles y Competencia Múltiple de Bogotá D.C.',
+  'Juzgado Ochenta y Tres Civil Municipal de Bogotá D.C. Transitorio Sesenta y Cinco (65) de Pequeñas Causas Civiles y Competencia Múltiple de Bogotá D.C.'
 ];
 
 export const juzgados = [
-  'Juzgado Ochenta y Tres (83) Municipal de Pequeñas Causas y Competencia Múltiple de Bogotá D.C.',
-  'Juzgado Sexto(6) de Pequeñas Causas Civiles y Competencia Múltiple de Bogotá DC',
-  'Juzgado Primero(1) Municipal de Pequeñas Causas Civiles y Competencia Múltiple de Tunja',
+  'Juzgado Sexto (6) de Pequeñas Causas Civiles y Competencia Múltiple de Bogotá D.C.',
+  'Juzgado Ochenta (80) Civil Municipal de Bogotá D.C. Transitorio Sesenta y Dos (62) de Pequeñas Causas Civiles y Competencia Múltiple de Bogotá D.C.',
+  'Juzgado Ochenta y Tres (83) Civil Municipal de Bogotá D.C. Transitorio Sesenta y Cinco (65) de Pequeñas Causas Civiles y Competencia Múltiple de Bogotá D.C.'
 ];
 
 export const procesos = [
-  'EJECUTIVO SINGULAR',
-  'EJECUTIVO SINGULAR DE MÍNIMA CUANTÍA'
+  'EJECUTIVO PARA LA EFECTIVIDAD DE LA GARANTÍA REAL',
+  'EJECUTIVO DE MÍNIMA CUANTÍA'
 ]
 
 export const ciudades = [
