fc131d1f7c58938315f8af89bb345b2804802d5c
Esteban1543 <jhoanestebanpv1@gmail.com>
2024-06-19 20:29:12 -0500

    Se agrego los endpoint para crear Usuario, Se agrego para activar y desactivar Usuario

--- backend/models/PersonasModel.js
+++ backend/models/PersonasModel.js
@@ -1,4 +1,5 @@
 import conexion from "../conection/conexion.js";
+import bcrypt from "bcrypt"
 
 class PersonaModel {
     static async getUsuarios(){
@@ -31,7 +32,91 @@ class PersonaModel {
                 type: String(error)
             }
         }
-    } 
+    }
+
+    static async newUsuario(fk_tipo_identificacion, n_identificacion, nombre_1, nombre_2, apellido_1, apellido_2, telefono, correo, alias, contraseña) {
+        try{
+            const hash = bcrypt.hashSync(contraseña, 10)
+
+            const [datos_persona] = await conexion.query(`
+                INSERT INTO datos_persona SET ?
+            `, {
+                fk_tipo_identificacion: fk_tipo_identificacion,
+                n_identificacion: n_identificacion,
+                nombre_1: nombre_1, 
+                nombre_2: nombre_2, 
+                apellido_1: apellido_1, 
+                apellido_2: apellido_2, 
+                telefono: telefono, 
+                correo: correo,
+                fk_tipo_persona: 2,
+                estado_persona: 1,
+            })
+
+            const [usuario] = await conexion.query(`
+                 INSERT INTO usuarios SET ?   
+            `,{
+                pfk_usuario: n_identificacion,
+                alias: alias,
+                contraseña: hash,
+                rol: 2,
+            })
+            
+            return {
+                status: true,
+                message: `✅ Se creo el usuario correctamente`,
+            }
+
+        }catch(error){
+            return {
+                status: false,
+                error: `⛔ Se genero un error interno con la base de datos`,
+                type: String(error)
+            }
+        }
+    }
+
+    static async deactivateUsuario(n_identificacion) {
+        try {
+            const [usuario] = await conexion.query(`
+                UPDATE datos_persona 
+                SET estado_persona = 0
+                WHERE n_identificacion = ? AND fk_tipo_persona = 2;
+            `, [n_identificacion])
+
+            return {
+                status: true,
+                message: "✅ Se desactivo persona correctamente",
+            }
+        }catch(error){
+            return {
+                status: false,
+                error: `⛔ Se genero un error interno con la base de datos`,
+                type: String(error)
+            }
+        }
+    }
+
+    static async activateUsuario(n_identificacion) {
+        try {
+            const [usuario] = await conexion.query(`
+                UPDATE datos_persona 
+                SET estado_persona = 1
+                WHERE n_identificacion = ? AND fk_tipo_persona = 2;
+            `, [n_identificacion])
+
+            return {
+                status: true,
+                message: "✅ Se activo persona correctamente",
+            }
+        }catch(error){
+            return {
+                status: false,
+                error: `⛔ Se genero un error interno con la base de datos`,
+                type: String(error)
+            }
+        }
+    }
 }
 
 export default PersonaModel
--- backend/routes/PersonasRouter.js
+++ backend/routes/PersonasRouter.js
@@ -6,4 +6,10 @@ import PersonaController from '../controllers/PersonasController.js'
 
 router.get('/usuarios', PersonaController.getUsuarios)
 
+router.post('/newUsuario', PersonaController.newUsuario)
+
+router.patch('/deactivateUsuario/:n_identificacion', PersonaController.deactivateUsuario)
+
+router.patch('/activateUsuario/:n_identificacion', PersonaController.activateUsuario)
+
 export default router;
